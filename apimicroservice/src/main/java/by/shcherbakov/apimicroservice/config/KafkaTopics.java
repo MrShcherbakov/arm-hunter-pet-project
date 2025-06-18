862071f5a0fa83f93c641ce63e6b6f4e1003950f
MrShcherbakov <celjdtham125@mail.ru>
2025-06-18 20:14:48 +0300

    create user.F.request,user.F.response topics

--- apimicroservice/src/main/java/by/shcherbakov/apimicroservice/config/KafkaTopics.java
+++ apimicroservice/src/main/java/by/shcherbakov/apimicroservice/config/KafkaTopics.java
@@ -1,4 +1,32 @@
 package by.shcherbakov.apimicroservice.config;
 
+import by.shcherbakov.apimicroservice.config.properties.KafkaTopicsProperties;
+import lombok.AllArgsConstructor;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+@AllArgsConstructor
 public class KafkaTopics {
+
+    private final KafkaTopicsProperties topicProps;
+
+    @Bean
+    public NewTopic userFindRequest() {
+        return TopicBuilder.name(topicProps.getUserFindRequest())
+                .replicas(2)
+                .partitions(3)
+                .build();
+    }
+
+    @Bean
+    public NewTopic userFindResponse() {
+        return TopicBuilder.name(topicProps.getUserFindResponse())
+                .replicas(2)
+                .partitions(3)
+                .build();
+    }
+
 }
